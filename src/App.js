53c12e2b61e13182c17b9c76613174c0a8144146
Pasha <prostobux@yandex.ru>
2020-03-17 23:12:40 +0300

    tic-tac-toe v1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function Squere(props) {
+    return (<button className="square" onClick={() => props.onClick()}>{props.value}</button>)
 }
 
-export default App;
+
+class Board extends Component {
+  renderSquare(i){
+    return <Squere value={this.state.squares[i]} onClick={() => this.handleClick(i)}></Squere>
+  }
+
+
+  handleClick(i){
+    const squares = this.state.squares.slice()
+    if (squares[i] || this.calculateWinner(this.state.squares)){
+      return
+    }
+    squares[i] =  this.state.xIsNext ? 'X' : 'O'
+    let xIsNext = !this.state.xIsNext
+    this.setState({squares : squares, xIsNext : xIsNext})
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      squares : Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+
+  render(){
+    const winner = this.calculateWinner(this.state.squares)
+    let status;
+    if (winner){
+        status = "Winner: " + winner
+    }
+    else{
+      status = 'Now turn: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
+    return(
+      <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+      </div>
+    </div>
+    )
+  }
+}
+
+class Game extends Component {
+  render(){
+    return(
+      <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default Game;
